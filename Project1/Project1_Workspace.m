2d86463fab17b1b3e23255b55a699aaa0694eaf5
danny28506 <danny28506@Starbase-28506>
2020-09-25 10:49:54 -0400

    Created Layer 4, works as expected.

--- Project1/Project1_Workspace.m
+++ Project1/Project1_Workspace.m
@@ -12,3 +12,6 @@ output_layer_2 = apply_convolve1(output_layer_1,filterbanks{1,2},biasvectors{1,2
 
 %First ReLU (Layer 3) - Points to function file "apply_relu.m"
 output_layer_3 = apply_relu(output_layer_2);
+
+%Second Convolution (Layer 4) - Points to function file "apply_convolve2.m"
+output_layer_4 = apply_convolve2(output_layer_3,filterbanks{1,4},biasvectors{1,4});
