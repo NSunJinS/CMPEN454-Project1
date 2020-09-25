54fb33e20ed87e266114b5cfddb8a7ac2a8d10d5
danny28506 <danny28506@Starbase-28506>
2020-09-25 12:00:46 -0400

    Layers 10, 11, & 12 Complete.

--- Project1/Project1_Workspace.m
+++ Project1/Project1_Workspace.m
@@ -31,3 +31,14 @@ output_layer_8 = apply_relu(output_layer_7);
 % 9th Layer - Fourth Convolution - Points to function file "apply_convolve.m"
 output_layer_9 = apply_convolve(output_layer_8,filterbanks{1,9},biasvectors{1,9});
 
+% 10th Layer - Fourth ReLU - Points to function file "apply_relu.m"
+output_layer_10 = apply_relu(output_layer_9);
+
+% 11th Layer - Second Maxpool - Points to function file "apply_maxpool1.m"
+output_layer_11 = apply_maxpool1(output_layer_10);
+
+% 12th Layer - Fifth Convolution - Points to function file "apply_convolve.m"
+output_layer_12 = apply_convolve(output_layer_11,filterbanks{1,12},biasvectors{1,12});
+
+
+
