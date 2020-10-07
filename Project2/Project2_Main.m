9b3a19e65f171efd729f1f37794922a9cab394ae
danny28506 <danny28506@Starbase-28506>
2020-10-07 09:12:14 -0400

    Added Main File and some comments.

---
+++ Project2/Project2_Main.m
@@ -0,0 +1,14 @@
+% EE454 Project 2 - Main
+% Group Members: Danny McClure, Nicholas Scarpitta, Weslee Hwang, & Mackenzie Myers
+
+% 1) Input/Parse Mocap Dataset
+
+% 2) Input/Parse Camera Paramters
+
+% 3) Project 3D to 2D Locations
+
+% 4) Triangulation of 2D to 3D 
+
+% 5) Computation of Error btwn Original and Triangulated 3D pts
+
+% 6) Compute Epipolar Lines
