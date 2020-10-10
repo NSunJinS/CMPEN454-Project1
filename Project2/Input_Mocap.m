961d8c44337658b40b96b2052215baec6b2287b2
danny28506 <danny28506@Starbase-28506>
2020-10-10 14:02:41 -0400

    Adding Function Files

---
+++ Project2/Input_Mocap.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Input_Mocap(inputArg1,inputArg2)
+%INPUT_MOCAP Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
