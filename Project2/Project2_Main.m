405a50d0b5a53edc3de528a0d7e4d86ffa501503
danny28506 <ddm5353@psu.edu>
2020-10-26 15:15:52 -0400

    Added World2Cam function

--- Project2/Project2_Main.m
+++ Project2/Project2_Main.m
@@ -1,25 +1,29 @@
 % EE454 Project 2 - Main
 % Group Members: Danny McClure, Nicholas Scarpitta, Weslee Hwang, & Mackenzie Myers
 
-% 1) Input/Parse Mocap Dataset
+% Input/Parse Mocap Dataset
+    mocap = 'Subject4-Session3-Take4_mocapJoints.mat';
+    mocap_full = open(mocap);
+    
+% Input/Parse Camera Paramters
 
-% 2) Input/Parse Camera Paramters
-
-% 3) Project 3D to 2D Locations
+% Project 3D to 2D Locations
     % Load in videos
     filenamevue2mp4 = 'Subject4-Session3-24form-Full-Take4-Vue2_updated.mp4';
     vue2video = VideoReader(filenamevue2mp4);
     
     filenamevue4mp4 = 'Subject4-Session3-24form-Full-Take4-Vue4_updated.mp4';
     vue4video = VideoReader(filenamevue4mp4);
-
     
+    % Convert World pts to Cam pts
+    campoint = world2cam(Pmat, worldpoint);
+    
+% Triangulation of 2D to 3D 
 
-% 4) Triangulation of 2D to 3D 
+% Computation of Error btwn Original and Triangulated 3D pts
 
-% 5) Computation of Error btwn Original and Triangulated 3D pts
-
-% 6) Compute Euclidean Distance
+% Compute Euclidean Distance
 L_Squared = Euclidean_Dist(point1, point2); % Function for computing L^2
 
-% 7) Compute Epipolar Lines
+
+% Compute Epipolar Lines
