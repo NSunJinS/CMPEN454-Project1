de6a6a2e68853449aa70a1c331f394d65b1c6cc8
NSunJinS <nss5267@psu.edu>
2020-11-01 00:46:46 -0400

    Added basic essential matrix extraction from the cam struct

--- Project2/project3DTo2D.m
+++ Project2/project3DTo2D.m
@@ -1,4 +1,26 @@
 function out = project3DTo2D(cam, worldCoord3DPoints) 
-out = worldCoord3DPoints;
+    printpoints2 = cam.prinpoint;
+    xprint2 = printpoints2(1);
+    yprint2 = printpoints2(2);
+    rotation2 = cam.Rmat;
+    position2 = cam.position;
+    focal_length2 =  cam.foclen;
+    pixelmat2 = [1 0 xprint2; 0 -1 yprint2; 0 0 1];
+    Pmat2 = cam.Pmat;
+    Pmat2 = [Pmat2;0 0 0 1];
+    
+    % Create Rotation Matrix [R]
+    rotation_matrix = [rotation2; 0,0,0];
+    temp = [0;0;0;1];
+    rotation_matrix = [rotation_matrix temp];
+    
+    % Create Skew Matrix [S]
+    skew_matrix = [1 0 0; 0 1 0; 0 0 1; 0 0 0];
+    position2 = position2*-1;
+    position2 = [position2 1]';
+    skew_matrix = [skew_matrix position2];
+
+    % Create Essential Matrix [E]
+    essential_matrix = rotation_matrix * skew_matrix;
 end
 
