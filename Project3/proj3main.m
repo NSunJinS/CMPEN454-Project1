ee7a1e348211ba7ee36a886231ebfdff7cb2334e
NSunJinS <nss5267@psu.edu>
2020-12-04 16:31:42 -0500

    Added function stubs to work on later and main project file

---
+++ Project3/proj3main.m
@@ -0,0 +1,21 @@
+% EE454 Project 2 - Main
+% Group Members: Danny McClure, Nicholas Scarpitta, Weslee Hwang, & Mackenzie Myers 
+
+
+% FOR GROUP: USE THIS AS THE FINAL TURN IN FUNCTION%%
+% dirstring = directory where numbered input image files are read from
+
+% maxframenum = integer which is the frame number of the last image file in the sequence
+
+% abs_diff_threshold = number between 0 and 255. It is the threshold called 
+        %“lambda” in the lecture slide pseudocode
+
+% alpha_parameter = floating point number between 0 and 1. It is the alpha
+        % “blending” parameter for adaptive
+        
+% gamma_parameter = number between 0 and 255. It is the gamma “decay” parameter used in
+        % the pseudocode for persistent frame differencing.
+function out = proj3main(dirstring,maxframenum,abs_doff_threshold,alpha_parameter,gamma_parameter) 
+
+
+end
