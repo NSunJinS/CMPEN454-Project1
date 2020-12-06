c97c88e0b031bdcf81cba6b09f50e64465ae65eb
NSunJinS <nss5267@psu.edu>
2020-12-05 20:22:59 -0500

    Added grayscale frame reading and a way to loop through the whole video

--- Project3/proj3main.m
+++ Project3/proj3main.m
@@ -19,8 +19,17 @@ function out = proj3main(dirstring,maxframenum,abs_diff_threshold,alpha_paramete
 
 %function stubs as per the project description
 
+v = VideoWriter(dirstring);
+open(v);
 %% read in each image in a loop and convert to grayscale
-
+for frame = 1:maxframenum
+    frameFile = sprintf('f%04d',frame);
+    im = imread(strcat(dirstring,frameFile,'.jpg'));
+    imGray = rgb2gray(im);
+    
+    writeVideo(v,imGray);
+end
+close(v);
 
 %% compute Background Subtraction
 
